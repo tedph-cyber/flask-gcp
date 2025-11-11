5a96559567c97ecb80788e78022154e59c2c914d
TeD <theooye7@gmail.com>
2025-11-12 00:20:01 +0100

    wow

--- flask-gcp/main.tf
+++ flask-gcp/main.tf
@@ -20,3 +20,23 @@ resource "google_storage_bucket" "gcp-flask" {
   name          = "flaskform-bucket"
   location      = "us-central-1"
  }
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "tf-vm"
+  machine_type = "e2-micro"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-12"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {}
+  }
+
+  metadata_startup_script = "echo Hello from Terraform > /var/www/html/index.html"
+}
+
